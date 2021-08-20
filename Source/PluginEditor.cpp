e12a9c10c4b9f9272c9d03729c97957190d6eb9f
Usama Ahmed <usiddiquie.bscs18seecs@seecs.edu.pk>
2021-08-20 23:38:38 +0500

    Added gain labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -325,7 +325,7 @@ void ResponseCurveComponent::resized()
 
     for (auto gDb : gain)
     {
-        auto y = jmap(gDb, -24.f, 24.f, float(getHeight()), 0.f);
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
 
         g.setColour(gDb  == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey);
 
@@ -359,6 +359,29 @@ void ResponseCurveComponent::resized()
         r.setSize(textWidth, fontHeight);
         r.setCentre(x, 0);
         r.setY(1);
+
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+
+    for (auto gDb : gain)
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+
+        String str;
+
+        if (gDb > 0) {
+            str << "+";
+        }
+        str << gDb;
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+
+        g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
 
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
