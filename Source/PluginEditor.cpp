3cb9ec7e0e27d3ee0bf4e406c1953ed8073f7164
Usama Ahmed <usiddiquie.bscs18seecs@seecs.edu.pk>
2021-08-21 18:20:01 +0500

    Analyzer button path is created only once

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -100,7 +100,7 @@ void LookAndFeel::drawToggleButton(juce::Graphics& g,
 
         g.drawEllipse(r, 2);
     }
-    else if (auto* pb = dynamic_cast<AnalyzerButton*>(&toggleButton))
+    else if (auto* analyzerButton = dynamic_cast<AnalyzerButton*>(&toggleButton))
     {
         auto colour = toggleButton.getToggleState() ? Colour(9u, 172u, 1u) : Colours::dimgrey;
 
@@ -109,20 +109,7 @@ void LookAndFeel::drawToggleButton(juce::Graphics& g,
         auto bounds = toggleButton.getLocalBounds();
         g.drawRect(bounds);
 
-        auto insetRect = bounds.reduced(4);
-
-        Path randomPath;
-
-        Random r;
-
-        randomPath.startNewSubPath(insetRect.getX(), insetRect.getY() + insetRect.getHeight() * r.nextFloat());
-
-        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
-        {
-            randomPath.lineTo(x, insetRect.getY() + insetRect.getHeight() * r.nextFloat());
-        }
-
-        g.strokePath(randomPath, PathStrokeType(1.f));
+        g.strokePath(analyzerButton->randomPath, PathStrokeType(1.f));
     }
      
 }
